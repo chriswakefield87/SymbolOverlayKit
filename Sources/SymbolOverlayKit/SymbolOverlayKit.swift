c15d1e0925d707924f885b84ef1aafa7580bd131
Chris Wakefield (Chris) <wakey7dev@gmail.com>
2024-02-23 09:46:23 +0000

    Cleaning code in progress

--- Sources/SymbolOverlayKit/SymbolOverlayKit.swift
+++ Sources/SymbolOverlayKit/SymbolOverlayKit.swift
@@ -22,13 +22,13 @@ struct SymbolItem: View {
 @available(iOS 14.0, macOS 11.0, *)
 public extension View {
     
-    @ViewBuilder func symbolOverlayRoundedRectangle(symbol: String, size: CGFloat, color: Color, opacity: Double, rotation: Double, spacing: CGFloat, rectangleCornerRadius: Double) -> some View {
-        var rows: [GridItem] { Array(repeating: GridItem(.fixed(10), spacing: spacing, alignment: .center), count: 15) }
+    @ViewBuilder func symbolOverlayRoundedRectangle(symbol: String, size: CGFloat, color: Color, opacity: Double, rotation: Double, vspacing: CGFloat, hspacing: CGFloat, rectangleCornerRadius: Double) -> some View {
+        var rows: [GridItem] { Array(repeating: GridItem(.fixed(10), spacing: vspacing, alignment: .center), count: 15) }
         
         self
             .overlay(
                 
-                LazyHGrid(rows: rows, alignment: .center, spacing: 25, pinnedViews: [], content: {
+                LazyHGrid(rows: rows, alignment: .center, spacing: hspacing, pinnedViews: [], content: {
                     ForEach(0 ..< 180) { item in
                         SymbolItem(color: color, symbol: symbol, size: size)
                     }
@@ -39,13 +39,13 @@ public extension View {
         
     }
     
-    @ViewBuilder func symbolOverlayRectangle(symbol: String, size: CGFloat, color: Color, opacity: Double, rotation: Double, spacing: CGFloat) -> some View {
-        var rows: [GridItem] { Array(repeating: GridItem(.fixed(10), spacing: spacing, alignment: .center), count: 15) }
+    @ViewBuilder func symbolOverlayRectangle(symbol: String, size: CGFloat, color: Color, opacity: Double, rotation: Double, vspacing: CGFloat, hspacing: CGFloat) -> some View {
+        var rows: [GridItem] { Array(repeating: GridItem(.fixed(10), spacing: vspacing, alignment: .center), count: 15) }
         
         self
             .overlay(
                 
-                LazyHGrid(rows: rows, alignment: .center, spacing: 25, pinnedViews: [], content: {
+                LazyHGrid(rows: rows, alignment: .center, spacing: hspacing, pinnedViews: [], content: {
                     ForEach(0 ..< 180) { item in
                         SymbolItem(color: color, symbol: symbol, size: size)
                     }
@@ -56,13 +56,13 @@ public extension View {
         
     }
     
-    @ViewBuilder func symbolOverlayCircle(symbol: String, size: CGFloat, color: Color, opacity: Double, rotation: Double, spacing: CGFloat) -> some View {
-        var rows: [GridItem] { Array(repeating: GridItem(.fixed(10), spacing: spacing, alignment: .center), count: 15) }
+    @ViewBuilder func symbolOverlayCircle(symbol: String, size: CGFloat, color: Color, opacity: Double, rotation: Double, vspacing: CGFloat, hspacing: CGFloat) -> some View {
+        var rows: [GridItem] { Array(repeating: GridItem(.fixed(10), spacing: vspacing, alignment: .center), count: 15) }
         
         self
             .overlay(
                 
-                LazyHGrid(rows: rows, alignment: .center, spacing: 25, pinnedViews: [], content: {
+                LazyHGrid(rows: rows, alignment: .center, spacing: hspacing, pinnedViews: [], content: {
                     ForEach(0 ..< 180) { item in
                         SymbolItem(color: color, symbol: symbol, size: size)
                     }
@@ -73,13 +73,13 @@ public extension View {
         
     }
     
-    @ViewBuilder func symbolOverlayCapsule(symbol: String, size: CGFloat, color: Color, opacity: Double, rotation: Double, spacing: CGFloat) -> some View {
-        var rows: [GridItem] { Array(repeating: GridItem(.fixed(10), spacing: spacing, alignment: .center), count: 15) }
+    @ViewBuilder func symbolOverlayCapsule(symbol: String, size: CGFloat, color: Color, opacity: Double, rotation: Double, vspacing: CGFloat, hspacing: CGFloat) -> some View {
+        var rows: [GridItem] { Array(repeating: GridItem(.fixed(10), spacing: vspacing, alignment: .center), count: 15) }
         
         self
             .overlay(
                 
-                LazyHGrid(rows: rows, alignment: .center, spacing: 25, pinnedViews: [], content: {
+                LazyHGrid(rows: rows, alignment: .center, spacing: hspacing, pinnedViews: [], content: {
                     ForEach(0 ..< 180) { item in
                         SymbolItem(color: color, symbol: symbol, size: size)
                     }
@@ -90,13 +90,13 @@ public extension View {
         
     }
     
-    @ViewBuilder func symbolOverlayEllipse(symbol: String, size: CGFloat, color: Color, opacity: Double, rotation: Double, spacing: CGFloat) -> some View {
-        var rows: [GridItem] { Array(repeating: GridItem(.fixed(10), spacing: spacing, alignment: .center), count: 15) }
+    @ViewBuilder func symbolOverlayEllipse(symbol: String, size: CGFloat, color: Color, opacity: Double, rotation: Double, vspacing: CGFloat, hspacing: CGFloat) -> some View {
+        var rows: [GridItem] { Array(repeating: GridItem(.fixed(10), spacing: vspacing, alignment: .center), count: 15) }
         
         self
             .overlay(
                 
-                LazyHGrid(rows: rows, alignment: .center, spacing: 25, pinnedViews: [], content: {
+                LazyHGrid(rows: rows, alignment: .center, spacing: hspacing, pinnedViews: [], content: {
                     ForEach(0 ..< 180) { item in
                         SymbolItem(color: color, symbol: symbol, size: size)
                     }
